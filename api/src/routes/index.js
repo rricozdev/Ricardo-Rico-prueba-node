94012d56c89828c74e82ea977aade6e160ff41d1
Ricardo Rico <programador5784@gmail.com>
2025-01-24 06:59:20 -0500

    Update index.js
    Comentarios a nuestro código.

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,9 +1,11 @@
-const { Router } = require("express");
-const routerProductos = require("./routerProductos");
+const { Router } = require("express"); // Importamos el enrutador de express
+const routerProductos = require("./routerProductos"); // Importamos el enrutador específico para productos
 
-const mainRouter = Router();
+const mainRouter = Router(); // Creamos ua instancia del enrutador principal.
 
-
+// Definimos la ruta base "/api" y asociammos el enrutador de productos.
 mainRouter.use("/api", routerProductos)
 
+
+// Exportamos el enrutador principal para que sea utilizado en otros arhivos.
 module.exports = mainRouter;
