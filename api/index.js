8e0e32fcea64a950f07a5b2e238de5649da96bf3
Ricardo Rico <programador5784@gmail.com>
2025-01-23 19:21:36 -0500

    Uptade index.js
    Actualizando manejo de errores en el index.js (entrada de la app)

---
+++ api/index.js
@@ -0,0 +1,13 @@
+const app = require("./src/app");
+const PORT = process.env.PORT ?? 3002;
+
+// Colocamos servidor a escuchar
+app.listen(PORT, () => {
+    console.log(`server listening on http://localhost:${PORT}`);
+});
+
+
+// Manejo de errores
+app.use((err, req, res, next) => {
+    res.status(500).send("Internal server error");
+});
