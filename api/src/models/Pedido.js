5c1bc24b1fd881c3b1ef1541a0196801fe59c8cb
Ricardo Rico <programador5784@gmail.com>
2025-01-23 20:23:25 -0500

    Update Pedido.js
    actualización del modelo Pedido.js

---
+++ api/src/models/Pedido.js
@@ -0,0 +1,70 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+    sequelize.define("Pedido", {
+        id: {
+            type: DataTypes.MEDIUMINT.UNSIGNED,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        instrucciones: {
+            type: DataTypes.STRING(500),
+            allowNull: true,
+        },
+        entrega_fecha: {
+            type: DataTypes.DATE,
+            allowNull: true,
+        },
+        valor_productos: {
+            type: DataTypes.DECIMAL(12, 3).UNSIGNED,
+            allowNull: false,
+        },
+        valor_envio: {
+            type: DataTypes.DECIMAL(10, 3).UNSIGNED,
+            allowNull: false,
+        },
+        valor_descuento: {
+            type: DataTypes.DECIMAL(12, 3).UNSIGNED,
+            allowNull: false,
+        },
+        valor_cupon: {
+            type: DataTypes.TINYINT.UNSIGNED,
+            allowNull: false,
+        },
+        valor_impuestos: {
+            type: DataTypes.DECIMAL(11, 3),
+            allowNull: false,
+        },
+        valor_final: {
+            type: DataTypes.DECIMAL(12, 3).UNSIGNED,
+            allowNull: false,
+        },
+        calificacion: {
+            type: DataTypes.DECIMAL(3, 2),
+            allowNull: true,
+        }, 
+        id_tienda: {
+            type: DataTypes.SMALLINT.UNSIGNED,
+            allowNull: false,
+        },
+        direccion: {
+            type: DataTypes.STRING(160),
+            allowNull: false,
+        },
+        valor_comision: {
+            type: DataTypes.MEDIUMINT.UNSIGNED,
+            allowNull: false,
+        },
+        id_user: {
+            type: DataTypes.MEDIUMINT.UNSIGNED,
+            allowNull: false,
+        },
+        created_at: {
+            type: DataTypes.DATE,
+            defaultValue: DataTypes.NOW,
+            allowNull: false,
+        },
+    },{
+        timestamps: false,
+    })
+}
