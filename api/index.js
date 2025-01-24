f38c2784a4f9034100c6d62f98394e66a7f7ede7
Ricardo Rico <programador5784@gmail.com>
2025-01-23 22:36:37 -0500

    Update index.js
    sincronizando db

--- api/index.js
+++ api/index.js
@@ -1,13 +1,16 @@
 const app = require("./src/app");
+const { conn } = require("./src/db");
+
 const PORT = process.env.PORT ?? 3002;
 
-// Colocamos servidor a escuchar
-app.listen(PORT, () => {
+conn.sync({}).then(() => {
+  // Colocamos servidor a escuchar
+  app.listen(PORT, () => {
     console.log(`server listening on http://localhost:${PORT}`);
+  });
 });
-
 
 // Manejo de errores
 app.use((err, req, res, next) => {
-    res.status(500).send("Internal server error");
+  res.status(500).send("Internal server error");
 });
