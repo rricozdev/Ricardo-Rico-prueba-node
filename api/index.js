3b077c90b5bc3ada1e7e823b7cc2b24a070ae262
Ricardo Rico <programador5784@gmail.com>
2025-01-24 15:32:23 -0500

    Update index.js
    realizando pruebas de data

--- api/index.js
+++ api/index.js
@@ -3,7 +3,9 @@ const { conn } = require("./src/db");
 
 const PORT = process.env.PORT ?? 3002;
 
-conn.sync({}).then(() => {
+conn.sync({force: false}).then(() => {
+  console.log("synchronized database");
+  
   // Colocamos servidor a escuchar
   app.listen(PORT, () => {
     console.log(`server listening on http://localhost:${PORT}`);
