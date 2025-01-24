ab2d9093fabc62a36b48b506270ddab3980973d4
Ricardo Rico <programador5784@gmail.com>
2025-01-24 15:32:46 -0500

    Update app.js
    configuramos cors

--- api/src/app.js
+++ api/src/app.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const mainRouter = require("./routes");
 
 const app = express();
 
 app.use(morgan("dev"));
+app.use(cors());
 app.use(express.json());
 
 app.use(mainRouter);
