276a65a46142ddc63d173c60aa6b7544c7ca3293
Ricardo Rico <programador5784@gmail.com>
2025-01-23 19:24:44 -0500

    Create Categoria.js
    Creación del modelo Categoria

---
+++ api/src/models/Categoria.js
@@ -0,0 +1,54 @@
+const { DataTypes } = require("sequelize");
+
+
+module.exports = (sequelize) => {
+    sequelize.define("Categoria", {
+        id: {
+            type: DataTypes.SMALLINT.UNSIGNED,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        estado: {
+            type: DataTypes.TINYINT.UNSIGNED,
+            allowNull: false,
+        }, 
+        nombre: {
+            type: DataTypes.STRING(30),
+            allowNull: false,
+        },
+        descripcion: {
+            type: DataTypes.STRING(500),
+            allowNull: true,
+        },
+        telefono: {
+            type: DataTypes.STRING(20),
+            allowNull: true,
+        },
+        direccion: {
+            type: DataTypes.STRING(1290),
+            allowNull:true,
+        },
+        direccion_anexo: {
+            type: DataTypes.STRING(40),
+            allowNull: true,
+        },
+        direccion_barrio: {
+            type: DataTypes.STRING(25),
+            allowNull: true,
+        },
+        calificacion: {
+            type: DataTypes.DECIMAL(3,2),
+            allowNull: true
+        },
+        impuestos: {
+            type: DataTypes.TINYINT.UNSIGNED,
+            allowNull:true,
+        },
+        dias_trabajados: {
+            type: DataTypes.STRING(21),
+            allowNull: true
+        },
+    },{
+        timestamps: false,
+    });
+};
