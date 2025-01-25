2773a4213a96c049d99329594f70fa82635db4cb
Ricardo Rico <programador5784@gmail.com>
2025-01-24 20:16:35 -0500

    Update index.js
    Sembrando productos

--- api/index.js
+++ api/index.js
@@ -1,14 +1,20 @@
 const app = require("./src/app");
-const { conn } = require("./src/db");
+const { conn, Producto } = require("./src/db");
+const productData = require("./productData");
 
 const PORT = process.env.PORT ?? 3002;
 
-conn.sync({force: false}).then(() => {
+conn.sync({ force: false }).then(() => {
   console.log("synchronized database");
-  
-  // Colocamos servidor a escuchar
-  app.listen(PORT, () => {
-    console.log(`server listening on http://localhost:${PORT}`);
+
+  Producto.bulkCreate(productData).then(() => {
+    console.log("20 products created succesfully");
+    // Colocamos servidor a escuchar
+    app.listen(PORT, () => {
+      console.log(`server listening on http://localhost:${PORT}`);
+    });
+  }).catch((err) => {
+    console.error("Error creating products:", err);
   });
 });
 
